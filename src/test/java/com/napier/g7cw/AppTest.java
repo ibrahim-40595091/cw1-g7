1c76d9186a0c830cd8c4b4df4c9e1b1d6b54de9b
Reece Smith <40872343+reeceasmith@users.noreply.github.com>
2024-03-13 17:06:30 +0000

    Created unit tests for database

--- src/test/java/com/napier/g7cw/AppTest.java
+++ src/test/java/com/napier/g7cw/AppTest.java
@@ -1,5 +1,6 @@
 package com.napier.g7cw;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -9,10 +10,27 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
-    static App app;
+    static DB db;
 
     @BeforeAll
     static void init() {
-        app = new App();
+        db = new DB();
+        db.connect();
+    }
+
+
+
+    @Test
+    void test_getCityPopulation() {
+        ArrayList<String> cities = App.getCitiesByPopulation();
+        assertNotNull(cities);
+        assertFalse(cities.isEmpty());
+    }
+
+
+
+    @AfterAll
+    static void de_init() {
+        db.disconnect();
     }
 }
