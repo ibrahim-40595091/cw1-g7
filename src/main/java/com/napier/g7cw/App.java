e1cf983e378999edf9b31960b8bea650b1a4f0c3
Reece Smith <40872343+reeceasmith@users.noreply.github.com>
2024-03-13 17:04:34 +0000

    Integrated SQL database into App method - created method getCitiesByPopulation() to test usage

--- src/main/java/com/napier/g7cw/App.java
+++ src/main/java/com/napier/g7cw/App.java
@@ -1,87 +1,45 @@
 package com.napier.g7cw;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class App {
-    static Connection con = null;
+    static DB db = null;
 
 
-
-    /**
-     * Connect to the MySQL database.
-     * Exits if MySQL JDBC driver is not found.
-     *
-     * @return boolean - True if connection was successful, false otherwise.
-     */
-    public static boolean db_connect() {
+    public static ArrayList<String> getCitiesByPopulation() {
+        ArrayList<String> cities = new ArrayList<String>();
         try {
-            // Load database driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("MySQL JDBC Driver not found! Error message: \n" + e.getMessage());
-            System.exit(-1);
-        }
-
-        // Connection to database
-        con = null;
-        int retries = 10;
-        for (int i = 0; i < retries; ++i) {
-            System.out.println("Connecting to database...");
-            try {
-                // Note: no longer need to wait for db to start - docker compose ensures this app only starts if db is running
-                // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://sql_db:3306/world?useSSL=false", "root", "password");
-                if (con != null) {
-                    System.out.println("Connected to database");
-                    return true;
-                }
-            } catch (SQLException e) {
-                System.out.println("Failed to connect to database on attempt " + i);
+            // Create an SQL statement
+            Statement stmt = db.con.createStatement();
+            // Create string for SQL statement
+            String strSelect = "SELECT Name FROM city ORDER BY Population DESC LIMIT 10";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new city if valid.
+            while (rset.next()) {
+                cities.add(rset.getString("Name"));
             }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get cities by population");
         }
-
-        return false;
-    }
-
-
-
-    /**
-     * Disconnect from the MySQL database.
-     *
-     * @return boolean - True if disconnection was successful, false otherwise.
-     */
-    public static boolean db_disconnect() {
-        if (con != null) {
-            try {
-                // Close connection
-                con.close();
-                return true;
-            } catch (Exception e) {
-                System.out.println("Error closing connection to database. Error message: \n" + e.getMessage());
-            }
-        }
-        return false;
+        return cities;
     }
 
 
 
     public static void main(String[] args)
     {
-        db_connect();
+        db = new DB();
+        db.connect();
 
         // Do stuff
-        String query = "SELECT Name FROM city WHERE CountryCode = 'GBR' ORDER BY Population DESC LIMIT 10";
-        try {
-            Statement stmt = con.createStatement();
-            ResultSet res = stmt.executeQuery(query);
-            while (res.next()) {
-                System.out.println(res.getString("Name"));
-            }
-        } catch (Exception e) {
-            System.out.println("Error executing query. Error message: \n" + e.getMessage());
+        ArrayList<String> cities = getCitiesByPopulation();
+        for (String city : cities) {
+            System.out.println(city);
         }
 
-
-        db_disconnect();
+        db.disconnect();
     }
 }
