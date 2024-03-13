44bbf07d4ea7b5b804aa1ffafcee31c6467e1334
Reece Smith <40872343+reeceasmith@users.noreply.github.com>
2024-03-13 02:12:59 +0000

    Added database control methods for App class, tested functionality in main method

--- src/main/java/com/napier/g7cw/App.java
+++ src/main/java/com/napier/g7cw/App.java
@@ -3,8 +3,17 @@ package com.napier.g7cw;
 import java.sql.*;
 
 public class App {
-    public static void main(String[] args)
-    {
+    static Connection con = null;
+
+
+
+    /**
+     * Connect to the MySQL database.
+     * Exits if MySQL JDBC driver is not found.
+     *
+     * @return boolean - True if connection was successful, false otherwise.
+     */
+    public static boolean db_connect() {
         try {
             // Load database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -14,34 +23,65 @@ public class App {
         }
 
         // Connection to database
-        Connection con = null;
+        con = null;
         int retries = 10;
         for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
             try {
-                // Wait for DB to start
-                Thread.sleep(1000);
+                // Note: no longer need to wait for db to start - docker compose ensures this app only starts if db is running
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "password");
+                con = DriverManager.getConnection("jdbc:mysql://sql_db:3306/world?useSSL=false", "root", "password");
                 if (con != null) {
                     System.out.println("Connected to database");
-                    break;
+                    return true;
                 }
             } catch (SQLException e) {
-                System.out.println("Failed to connect to database on attempt " + Integer.toString(i));
-                System.out.println(e.getMessage());
-            } catch (InterruptedException e) {
-                System.out.println("Thread interrupted. Error message: \n" + e.getMessage());
+                System.out.println("Failed to connect to database on attempt " + i);
             }
         }
 
+        return false;
+    }
+
+
+
+    /**
+     * Disconnect from the MySQL database.
+     *
+     * @return boolean - True if disconnection was successful, false otherwise.
+     */
+    public static boolean db_disconnect() {
         if (con != null) {
             try {
                 // Close connection
                 con.close();
+                return true;
             } catch (Exception e) {
                 System.out.println("Error closing connection to database. Error message: \n" + e.getMessage());
             }
         }
+        return false;
+    }
+
+
+
+    public static void main(String[] args)
+    {
+        db_connect();
+
+        // Do stuff
+        String query = "SELECT Name FROM city WHERE CountryCode = 'GBR' ORDER BY Population DESC LIMIT 10";
+        try {
+            Statement stmt = con.createStatement();
+            ResultSet res = stmt.executeQuery(query);
+            while (res.next()) {
+                System.out.println(res.getString("Name"));
+            }
+        } catch (Exception e) {
+            System.out.println("Error executing query. Error message: \n" + e.getMessage());
+        }
+
+
+        db_disconnect();
     }
 }
