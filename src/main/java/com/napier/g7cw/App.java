3f83bf6c25813c789b61446b5051036620be5312
Reece Smith <40872343+ReeceASmith@users.noreply.github.com>
2024-03-19 17:11:44 +0000

    Testing reporting class

--- src/main/java/com/napier/g7cw/App.java
+++ src/main/java/com/napier/g7cw/App.java
@@ -3,30 +3,13 @@ package com.napier.g7cw;
 import com.napier.g7cw.db.CountryDBA;
 import com.napier.g7cw.db.DB;
 import com.napier.g7cw.obj.Country;
+import com.napier.g7cw.obj.Report;
 
 import java.util.HashMap;
 
 public class App {
     static DB db = null;
 
-
-    /**
-     * Generates a capital city report using HashMap data taken from the database
-     * @param data
-     * The data fetched from the database using class DB
-     * @return String
-     * The report generated as a String
-     */
-    public static String generateReport(HashMap<String, String> data)
-    {
-        String out;
-        out = "Capital City Report:\n" +
-                "\tName: " + data.get("Name") + "\n" +
-                "\tCountry: " + data.get("Country") + "\n" +
-                "\tPopulation: " + data.get("Population") + "\n";
-
-        return out;
-    }
 
 
     public static void main(String[] args) {
@@ -41,6 +24,10 @@ public class App {
         Country a = CountryDBA.getCountry(db, "name", "United Kingdom");
         System.out.println("Country Name: " + a.LocalName);
         System.out.println("Testing");
+        Report report = new Report(db);
+        report.generate(a.Capital);
+        report.display();
+
 
         db.disconnect();
     }
