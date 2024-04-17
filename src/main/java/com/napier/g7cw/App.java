960bad553ba06347fc77d0d9875464e30b9d71f1
Reece Smith <40872343+ReeceASmith@users.noreply.github.com>
2024-04-17 21:56:18 +0100

    Added city/country unit tests

--- src/main/java/com/napier/g7cw/App.java
+++ src/main/java/com/napier/g7cw/App.java
@@ -22,10 +22,31 @@ public class App {
             db.connect(args[0], Integer.parseInt(args[1]));
         }
 
+
         Report report = new Report(db);
         report.getContinentCapitalCitiesSortPopulation("Europe", true, 10);
         report.display();
 
         db.disconnect();
+    }
+
+
+    public static int getMultichoice(String question, ArrayList<String> answers) {
+        int choiceInt;
+        do {
+            System.out.println(question);
+            for (int i = 0; i < answers.size(); i++) {
+                System.out.println("[" + (i + 1) + "]\t" + answers.get(i));
+            }
+            System.out.print("Choice: ");
+            String choice = System.console().readLine();
+            try {
+                choiceInt = Integer.parseInt(choice);
+            } catch (NumberFormatException e) {
+                choiceInt = -1;
+            }
+        } while (choiceInt < 0 || choiceInt > answers.size());
+
+        return choiceInt;
     }
 }
--- src/test/java/com/napier/g7cw/AppTest.java
+++ src/test/java/com/napier/g7cw/AppTest.java
@@ -1,10 +1,13 @@
 package com.napier.g7cw;
 
 import com.napier.g7cw.db.CityDBA;
+import com.napier.g7cw.db.CountryDBA;
 import com.napier.g7cw.db.DB;
 import com.napier.g7cw.obj.City;
+import com.napier.g7cw.obj.Country;
 import com.napier.g7cw.obj.Report;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,29 +18,47 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class AppTest {
-
-
-
-    // Keeping this code in case we need it in future
-    // Intention is to test whether the DB is connecting/returning data/disconnecting correctly
-    // Currently only testing whether the functions can process the raw DB data correctly
     static DB db;
-/*
-    @BeforeEach
-    void init() {
+
+    @BeforeAll
+    static void init() {
         db = new DB();
         db.connect("localhost:33060", 10000);
-        App.db = db;
     }
-*/
 
 
+    @Test
+    void testGetCity() {
+        City city = CityDBA.getCityByID(db, 1);
+        assertNotNull(city);
+        assertEquals(city.ID, 1);
+        assertEquals(city.Name, "Kabul");
+        assertEquals(city.CountryCode, "AFG");
+        assertEquals(city.District, "Kabol");
+        assertEquals(city.Population, 1780000);
+    }
 
-    /*
+    @Test
+    void testGetCityNull() {
+        City city = CityDBA.getCityByID(db, 0);
+        assertEquals(city, City.NULL_CITY);
+    }
+
+    @Test
+    void testGetCountry(){
+        Country country = CountryDBA.getCountry(db, "AFG");
+        assertNotNull(country);
+        assertEquals(country.Code, "AFG");
+        assertEquals(country.Name, "Afghanistan");
+        assertEquals(country.Continent.Name, "Asia");
+        assertEquals(country.Region, "Southern and Central Asia");
+        assertEquals(country.Population, 22720000);
+        assertEquals(country.Capital.CountryCode, "AFG");
+    }
+
+
     @AfterAll
-    static void de_init() {
+    static void tearDown() {
         db.disconnect();
     }
-*/
-
 }
